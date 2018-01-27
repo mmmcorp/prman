9edec4fd37d26e9f9a07fd86dd0f09319c7195e7
ygnmhdtt <yaginuma@mmmcorp.co.jp>
2018-01-28 02:04:22 +0900

    fix struct and method

--- pull_request.go
+++ pull_request.go
@@ -5,15 +5,16 @@ import "strings"
 type pullRequests []pullRequest
 
 type pullRequest struct {
-	HTMLURL            string `json:"html_url"`
-	Title              string `json:"title"`
-	RequestedReviewers users  `json:"requested_reviewers"`
+	HTMLURL string `json:"html_url"`
+	Title   string `json:"title"`
+	Number  int    `json:"number"`
+	Body    string `json:"body"`
 }
 
 func (pr *pullRequest) isWIP() bool {
 	return strings.HasPrefix(pr.Title, "WIP") || strings.HasPrefix(pr.Title, "(WIP)") || strings.HasPrefix(pr.Title, "[WIP]") || strings.HasPrefix(pr.Title, "【WIP】")
 }
 
-func (pr *pullRequest) isContainingAsReviewer(user string) bool {
-	return pr.RequestedReviewers.contains(user)
+func (pr *pullRequest) mustReviewedBy(user string) bool {
+	return strings.Contains(pr.Body, user)
 }
