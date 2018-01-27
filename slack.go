30227a2de3e87326f122b28f07539b7b1839d377
ygnmhdtt <yaginuma@mmmcorp.co.jp>
2018-01-27 19:16:19 +0900

    fix make enable to get argument

--- slack.go
+++ slack.go
@@ -11,11 +11,21 @@ func getUserFromSlashRequest(r *http.Request) (string, error) {
 	bbody, _ := ioutil.ReadAll(r.Body)
 	body := string(bbody)
 	arr := strings.Split(body, "&")
-	for _, elem := range arr {
-		if strings.HasPrefix(elem, "user_name") {
-			// elem will be user_name=yagi
-			return strings.Split(elem, "=")[1], nil
-		}
+	if getText(arr, "text") != "" {
+		return getText(arr, "text"), nil
+	}
+	if getText(arr, "user_name") != "" {
+		return getText(arr, "user_name"), nil
 	}
 	return "", errors.New("user_nameパラメータがありません。")
+}
+
+func getText(source []string, target string) string {
+	for _, elem := range source {
+		if strings.HasPrefix(elem, target) {
+			arr := strings.Split(elem, "=")
+			return arr[1]
+		}
+	}
+	return ""
 }
