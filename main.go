b5144dec3cf6f1b80b33243e461bc925a0ca0631
ygnmhdtt <yaginuma@mmmcorp.co.jp>
2018-01-27 18:27:40 +0900

    fix main

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-// PullRequestServer serves pull requests
-func PullRequestServer(w http.ResponseWriter, r *http.Request) {
-	bufbody := new(bytes.Buffer)
-	bufbody.ReadFrom(r.Body)
-	body := bufbody.String()
-	fmt.Fprintf(w, body)
-	fmt.Fprintf(w, "Hello, World\n")
+var c *client
+
+func pullRequestController(w http.ResponseWriter, r *http.Request) {
+	su, err := getUserFromSlashRequest(r)
+	if err != nil {
+		log.Println(err)
+	}
+
+	githubUser, err := convertSlackUserNameToGithubUserName(su)
+	if err != nil {
+		log.Println(err)
+	}
+
+	c = newClient(os.Getenv("PR_GITHUB_ORG"), os.Getenv("PR_GITHUB_TOKEN"))
+	repos, err := c.getRepos()
+	if err != nil {
+		log.Println(err)
+	}
+
+	response, err := repos.getPullRequestsReviewRequestedFor(githubUser)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, fmt.Sprintf("{\"response_type\":\"in_channel\",\"text\":\"%v\"}", response))
 }
+
 func main() {
-	http.HandleFunc("/", PullRequestServer)
+	http.HandleFunc("/", pullRequestController)
 	http.ListenAndServe(":7000", nil)
 }
